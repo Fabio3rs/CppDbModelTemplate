93b4133e7530f591ea4b9ce770d1d54f5b8f8b50
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2021-12-08 20:06:26 -0300

    Dump json first commit [to be tested]

--- template.hpp.php
+++ template.hpp.php
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <Poco/JSON/JSON.h>
+#include <Poco/Dynamic/Var.h>
+
 #include "../Database/GenericDBConnection.hpp"
 #include <chrono>
 #include <memory>
 #include <optional>
+#include <string>
 
 class <?= $MODEL_NAME ?> {
     std::shared_ptr<GenericDBConnection> usingconn;
@@ -28,6 +32,32 @@ public:
 
     auto findByID(uint64_t fid) -> bool;
     void save();
+
+    auto dump_json() -> Poco::JSON::Object::Ptr
+    {
+        Poco::JSON::Object::Ptr result(new Poco::JSON::Object);
+
+        result->set("id", empty()? Poco::Dynamic::Var() :  Poco::Dynamic::Var(id));
+        
+        <?php
+        foreach ($FIELDS as $k => $v)
+        {
+            if ($v->nullable)
+            {
+                ?>
+                result->set("<?= $v->name ?>", <?= $v->name ?>? Poco::Dynamic::Var(<?= $v->name ?>.value()) :  Poco::Dynamic::Var());
+                <?php
+            }else
+            {
+                ?>
+                result->set("<?= $v->name ?>", <?= $v->name ?>);
+                <?php
+            }
+        }
+        ?>
+
+        return result;
+    }
 
     <?= $MODEL_NAME ?>(std::shared_ptr<GenericDBConnection> conn)
         : usingconn(conn) {}
